00ad20a41173b8b86b4709ff5823ce52d63e6c7d
imrushabhshah <rushabhmanoj@gmail.com>
2019-11-17 23:19:18 -0600

    lstm attack added

--- attack_utils.py
+++ attack_utils.py
@@ -4,6 +4,9 @@ from nltk.corpus import wordnet
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
+import random
+MAX_SEQUENCE_LENGTH = 5000
+
 
 
 def get_tokens(sent):
@@ -48,3 +51,59 @@ def get_synonymes(word):
     syn_dict[t].add(w)
   return sss,syn_dict
 
+def predict_sentence(model,sent):
+    try_vector = tokenizer.texts_to_sequences([sent])
+    try_vector = pad_sequences([try_vector[0]], 
+                     maxlen=MAX_SEQUENCE_LENGTH, 
+                     padding='pre', 
+                     truncating='pre')
+    val = model.predict(try_vector)
+    return val
+
+def attack(model,dummy,pertub=1, printSwaps = False):    
+    dummy_temp = nltk.word_tokenize(dummy[:MAX_SEQUENCE_LENGTH])
+    done = False
+    unsuccessfullSwaps = 0
+    for i in range(int(len(dummy_temp)*pertub)):
+        if done:
+            break
+        itter = 0
+        while itter<10000:
+            itter+=1
+            v = random.randint(0,len(dummy_temp)-1)
+            syns,_ = get_synonymes(dummy_temp[v])
+            if len(syns)>0:
+                break
+        if itter == 10000:
+            print("didnt catch any synonymes")
+            continue
+        candidates = {}
+        # candidate is a scored candidate dictionary storing all the 
+        # synonymes with prediction Score
+        word = dummy_temp[v]
+        sent = " ".join(dummy_temp)
+        candidates[word]=predict_sentence(model,sent)
+        for s in syns:
+            dummy_temp[v] = s
+            sent = (" ".join(dummy_temp))
+            val = predict_sentence(model,sent)
+            candidates[s] = val
+            if val>=0.5:
+#                 print("got changes in ",i,val)
+                done = True
+                break
+    #         best_candidate = max(candidates.iteritems(), key=operator.itemgetter(1))[0]
+        best_candidate = max(candidates, key=candidates.get)
+        if word == best_candidate:
+            unsuccessfullSwaps+=1
+#             if printSwaps:
+#                 print("did not swap")
+#         else:
+#             if printSwaps:
+#                 print(word,best_candidate)
+        dummy_temp[v] = best_candidate
+    if not done:
+#         print("sorry")
+        return 0
+    print("swaps done ",i+1-unsuccessfullSwaps,"total words",len(dummy_temp),"unsucessfull swaps try",unsuccessfullSwaps)
+    return i+1-unsuccessfullSwaps
